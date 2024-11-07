05ce63cc8e88bf4f717949113a7dd031cb10f582
Alekssita <113428804+Alekssita@users.noreply.github.com>
2024-11-07 23:44:11 +0100

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+function makeTransaction(quantity, pricePerDroid) {
+    quantity = Number(quantity);
+    pricePerDroid = Number(pricePerDroid);
+    console.log (`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+   
+   
+}
+makeTransaction();
